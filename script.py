ffc0643ff8b9840f91948fb5f940634348b3b2bb
Brohmarr <panza.thiagoj@gmail.com>
2023-09-22 19:52:35 -0300

    Add initial file with the instructions

---
+++ script.py
@@ -0,0 +1,31 @@
+# Project: Python CLI Potion Shop
+# by: @Brohmarr
+
+# Welcome! You should now be equipped with everything you need to make a
+#     complicated program that uses multiple classes that interact with
+#     each other. This is where we turn the creativity over to you.
+# 
+# For this project, we want you to create a fully functional system that
+#     involves multiple classes. We’ll give more details on the specific
+#     requirements below.
+# 
+# We know that it can sometimes be hard to come up with an idea on your
+#     own. Take some time to think of what you want to create — it truly
+#     can be anything. This step is important! We want you to feel
+#     motivated to complete this project, so pick something you’re
+#     excited to make.
+
+# Requirements:
+# 
+# [ ] (1) You must create at least TWO classes;
+# [ ] (2) Each of those classes must have at least THREE attributes and
+#             THREE methods;
+# [ ] (3) Your classes should be able to describe themselves (through a
+#             __repr__() method);
+# [ ] (4) Test all of the methods you created on at least TWO instances
+#             of every one of your classes;
+# [ ] (5) Create some methods, or additional attributes, that make your
+#             two Classes able to interact with each other.
+
+
+# Code here...
