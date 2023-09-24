cbf5c7c92ddc9f384c3fcd6c204a7fa32f19f0f5
Brohmarr <panza.thiagoj@gmail.com>
2023-09-24 19:10:22 -0300

    Add logic to manage the shop's inventory and code cleanup

--- script.py
+++ script.py
@@ -39,20 +39,6 @@ class Shopkeeper:
         return "This is {name}, the {job}.".format(
             name = self.name,
             job = self.job
-        ) + " He doesn't seem very friendly..."
-
-# This class represents the potion shop itself.
-class PotionShop:
-    def __init__(self, name: str, owner: Shopkeeper):
-        self.name = name
-        self.owner = owner
-        # {(key = item_name: str): (value = quantity_in_stock: int)}
-        self.inventory = {}
-    
-    def __repr__(self):
-        return "This is {owner}'s potion shop, {name}!".format(
-            owner = self.owner.name,
-            name = self.name
         )
 
 # This class represents the items in the potion shop.
@@ -68,6 +54,54 @@ class Item():
             level = self.level,
             price = self.price
         ) + " gold pieces."
+
+# This class represents the potion shop itself.
+class PotionShop:
+    def __init__(self, name: str, owner: Shopkeeper):
+        self.name = name
+        self.owner = owner
+        # {(key = item_name: str): (value = quantity_in_stock: int)}
+        self.inventory = {}
+    
+    def __repr__(self):
+        return "This is {owner}'s potion shop, {name}!".format(
+            owner = self.owner.name,
+            name = self.name
+        )
+    
+    def is_item_in_stock(self, item:Item):
+        if item.name in self.inventory:
+            return True
+        else:
+            return False
+
+    def add_to_inventory(self, item: Item, amount: int):
+        if self.is_item_in_stock(item):
+            self.inventory[item.name] += amount
+        else:
+            self.inventory[item.name] = amount
+        print("Added {amount}x [{item_name}] to the stock!".format(
+                amount = amount,
+                item_name = item.name
+            ))
+    
+    def remove_from_inventory(self, item: Item, amount: int):
+        if item.name in self.inventory:
+            if self.inventory[item.name] >= amount:
+                if self.inventory[item.name] > amount:
+                    self.inventory[item.name] -= amount
+                else:
+                    self.inventory.pop(item.name)
+                print("Here you go, {amount}x [{item_name}]!".format(
+                    amount = amount,
+                    item_name = item.name
+                ))
+            else:
+                print("I can only sell you {inventory_amount}".format(
+                    inventory_amount = self.inventory[item.name]
+                ) + " of that item. Would that be enough, adventurer?")
+        else:
+            print("Unfortunatelly, I'm out of that one.")
 
 # This class represents an adventurer (basically a client).
 class Adventurer:
@@ -86,17 +120,37 @@ class Adventurer:
 print("\nInitializing Internal Testing...\n")
 
 print("Testing '__init__' and '__repr__' methods...\n")
+print("---\n")
 
 shopkeeper = Shopkeeper("Guy", 50)
 print(shopkeeper)
 
-potion_shop = PotionShop("Potion Place", shopkeeper)
-print(potion_shop)
-
 potion_of_health = Item("Potion of Health", 99, 500)
 print(potion_of_health)
+
+potion_shop = PotionShop("Potion Place", shopkeeper)
+print(potion_shop)
 
 adventurer = Adventurer("Chad", "Fighter", 500)
 print(adventurer)
 
 print("\nGreat success!\n")
+print("---\n")
+
+print("Testing the shop's inventory management...\n")
+print("---\n")
+
+potion_shop.add_to_inventory(potion_of_health, 1)
+print()
+potion_shop.remove_from_inventory(potion_of_health, 2)
+print()
+potion_shop.add_to_inventory(potion_of_health, 1)
+print()
+print(potion_shop.inventory)
+print()
+potion_shop.remove_from_inventory(potion_of_health, 2)
+print()
+print(potion_shop.inventory)
+
+print("\nGreat success!\n")
+print("---\n")
